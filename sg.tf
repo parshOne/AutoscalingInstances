ee1d805ae3e19a9b9357e4a1284554da27d29f30
Parvesh Negi <77884873+parshOne@users.noreply.github.com>
2023-06-25 00:25:48 +0530

    Updating dynamic block

--- sg.tf
+++ sg.tf
@@ -10,8 +10,8 @@ resource "aws_security_group" "ec2_public_security_group" {
    for_each = "${var.ports}"
    iterator = port
     content {
-      from_port   = "${port.value}
-      to_port     = 22
+      from_port   = port.value
+      to_port     = port.value
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
